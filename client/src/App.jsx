225bfbc0b81a5103810eab9eb35934eb0602fbae
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-04-10 14:07:14 +0530

    vg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,69 @@
-const App = () => {
-  return (
-    <div className='p-5'>
-      <strong className='text-2xl block'>Technologies</strong><br />
-      <ul className='list-disc pl-5'>
-        <li>Tailwind CSS</li>
-        <li>Chakra-UI</li>
-        <li>Redux-Toolkit</li>
-      </ul>
-    </div>
-  )
+/* eslint-disable no-unused-vars */
+import { Outlet, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Chat from './Pages/chat/Chat';
+import Navbar from './components/Navbar';
+import Matchmaking from './Pages/Matchmaking';
+import Profile from './Pages/Profile';
+import Settings from './Pages/Settings';
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setTheme, toggleTheme } from './slice/userSlice';
+import { useColorMode } from '@chakra-ui/react';
+import Login from './Pages/login/Login';
+import Signup from './Pages/signup/Signup';
+import { auth } from './Firebase/Firebase';
+import { loginUser, setLoading } from './features/userSlice';
+
+
+function Layout() {
+    return (
+        <div className="flex h-screen">
+            <Navbar />
+            <Outlet />
+        </div>
+    );
 }
 
-export default App
+const App = () => {
+    const userInfo = useSelector((state => state.user))
+    const dispatch = useDispatch();
+    const { colorMode, toggleColorMode } = useColorMode()
+
+    useEffect(() => {
+        dispatch(setTheme(colorMode))
+        auth.onAuthStateChanged(authUser => {
+            if(authUser){
+                dispatch(loginUser({
+                    uid: authUser.uid,
+                    username: authUser.displayName,
+                    email: authUser.email
+                })
+                );
+                dispatch(setLoading(false));
+            }else{
+                console.log("user is not logged in");
+            }
+        })
+    });
+
+    const handleThemeToggle = () => {
+        dispatch(toggleTheme())
+        toggleColorMode()
+    }
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path="/matchmaking" element={<Matchmaking />} />
+                <Route path="/chat" element={<Chat />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/settings" element={<Settings />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+        </Routes>
+    );
+};
+
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import { ColorModeScript } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from 'react-redux'
+import {store} from './store/store.js'
+import { BrowserRouter } from 'react-router-dom'
+import theme from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider>
+  <Provider store={store}>
+ <React.StrictMode>
+   <BrowserRouter>
+
+    <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
+  
+    </BrowserRouter>
+  </React.StrictMode>
+  </Provider>
 )
+
