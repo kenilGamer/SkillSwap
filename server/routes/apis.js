8ace20570b04c39a18b72ce3c5ea5b8e9a486ca5
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-04-11 15:54:44 +0530

    remove backendfiles

--- server/routes/apis.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/dd', function(req, res, next) {
-    res.json({username: 'kenil111',id: '22244', gmail: "kenil@gmail.com"})
-});
-
-module.exports = router;
